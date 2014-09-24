334b4e4ff2b5a98e92cf3a531d886f9fd2462465
Jiannan Zhang <jz1992@cs.utexas.edu>
2014-09-23 22:37:15 -0500

    fixed the bug and run well

--- Netflix.py
+++ Netflix.py
@@ -2,15 +2,21 @@ import json
 
 # get prediction given mvId, userID
 # mvID and userID are strs
+def getAveAllUsers(userCacheDic):
+    sumRating = 0
+    length = len(userCacheDic)
+    for key in userCacheDic:
+        sumRating += float(userCacheDic[key])
+    return sumRating / length
+
+userCacheDic = json.load(open('/u/prat0318/netflix-tests/ctd446-userAverageRating.txt','r'))
+movieCacheDic = json.load(open('/u/prat0318/netflix-tests/ctd446-movieAverageRating.txt','r'))
+AveAllUsers = getAveAllUsers(userCacheDic)
+
 def getPredictRating(mvID,userID):
     #cacheList = os.listdir(/u/prat0318/netflix-tests/)
-    # caches are dics
     #movieCache = open(os.listdir(/u/prat0318/netflix-tests/ctd446-movieAverageRating.txt))
-    movieCacheDic = json.load(open('/u/prat0318/netflix-tests/ctd446-movieAverageRating.txt','r'))
 
-    userCacheDic = json.load(open('/u/prat0318/netflix-tests/ctd446-userAverageRating.txt','r'))
-
-    AveAllUsers = getAveAllUsers(userCacheDic)
     # movieOff and userOff
     movieOff = float(movieCacheDic[mvID]) - AveAllUsers
     userOff = float(userCacheDic[userID]) - AveAllUsers
@@ -28,12 +34,12 @@ def netflixEval(r,w):
         # first check it is not : in order to go to else clasue to record the id
         if (line[-1] == ':'):
             mvIDEval = line[:-1]
-            w.write(line)
+            w.write(line + '\n')
         # record id and then loop each user
         else:
             userIDEval = line
             predictRat = getPredictRating(mvIDEval,userIDEval)
-            w.write(str(predictRat))
+            w.write(str(predictRat) + '\n')
 
 
 
@@ -43,20 +49,7 @@ def netflixEval(r,w):
 # movieOff = aveMovieRat - aveAllUserating
 
 # first find the averating for all users
-def getAveAllUsers(userCacheDic):
-    sumRating = 0
-    length = len(userCacheDic)
-    for key in userCacheDic:
-        sumRating += float(userCacheDic[key])
-    return sumRating / length
-    '''
-    times = 0
-    for line in cacheUserFile:
-        stList = line.split()
-        times += 1
-        sumRating += stList[1]
-    return sumRating / times
-    '''
+
 
 
 
